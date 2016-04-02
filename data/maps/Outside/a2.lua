44c21d9e63524e340bdcd6f6eb038bb25341975c
GameBoyArda <arda.basutcu123@gmail.com>
2016-04-02 09:48:28 +0200

    Added files via upload

--- data/maps/Outside/a2.lua
+++ data/maps/Outside/a2.lua
@@ -1,1 +1,8 @@
 local map = ...
+
+local map = ...
+
+function a_switch:on_activated()
+
+  map:set_entities_enabled("go_tile_", false)
+end
--- data/maps/Outside/a4.lua
+++ data/maps/Outside/a4.lua
@@ -1,1 +1,6 @@
 local map = ...
+
+function a_switch:on_activated()
+
+  map:set_entities_enabled("go_tile", false)
+end
